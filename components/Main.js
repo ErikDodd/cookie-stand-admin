0cd53c3287168b360ba4224ee95de9fc51349bbc
Erik M. Dodd <emdodd27@gmail.com>
2023-02-08 18:06:25 -0800

    Working on Cookie Stand Table

--- components/Main.js
+++ components/Main.js
@@ -6,7 +6,7 @@ export default function Main(props) {
         <main className='bg-gray-200'>
             <h2 className='flex w-3/4 p-2 mx-auto my-4 bg-blue-400 justify-center rounded-lg mb-0'> Create Cookie Stand</h2>
             <Form cookieFormHandler={props.cookieFormHandler} />
-            <ReportTable tableHeaders={props.tableHeaders}/>
+            <ReportTable tableHeaders={props.tableHeaders} cookieStoreCreateds={props.cookieStoreCreateds}/>
             <p className='flex w-3/4 p-2 mx-auto my-4 rounded-lg justify-center'> Report Table Coming Soon...</p>
             <p className='flex justify-center'> {'{"location": "Barcelona, "minCustomers": 2, "maxCustomers":4, "avgCookies": 2.5}'} </p>
         </main>
